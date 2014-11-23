83ae1a3e81e5db48d4c15cd9993b16d534923aaa
Alexey Zaslavsky <pterolex@yandex.ru>
2014-11-23 22:27:36 +0200

    Finish proof of concept

--- src/UI.jsx
+++ src/UI.jsx
@@ -5,20 +5,83 @@
 'use strict';
 
 var React = require('react');
+var _     = require('lodash');
 
 require('./UI.less');
+
+var questions = require('./questions.json');
+
+var cars = {
+    citroen:  0,
+    hyundai:   0,
+    mercedes: 0,
+};
+
+var carName = {
+    citroen: 'Citroen',
+    hyundai: 'Hyundai',
+    mercedes: 'Mercedes'
+};
 
 var UI = React.createClass({
     componentDidMount() {
 
     },
 
+    getInitialState() {
+        return {
+            currentQuestion: 0
+        };
+    },
+
+    _getNextQuestion() {
+        if (this.state.currentQuestion < questions.length-1) {
+            this.setState({
+                currentQuestion: this.state.currentQuestion + 1
+            });
+        }
+    },
+
+    _answerQuestion(number) {
+        var points = questions[this.state.currentQuestion].answers[number].points;
+
+        for ( var car in points ) {
+            cars[ car ] += points[ car ];
+        }
+        console.log(number,cars);
+        this._getNextQuestion();
+    },
+
     render() {
+        console.log('currentQuestion',this.state.currentQuestion);
+        var question = questions[this.state.currentQuestion];
+        var isQuizFinished = this.state.currentQuestion == questions.length-1;
+
+        var answersNode = question.answers.map( (answer,i) => <button onClick={this._answerQuestion.bind(this,i)}>{answer}</button>);
+
+        var questionNode = (
+            <div className='question'>
+                <div className='text'>
+                    {question.text}
+                </div>
+                {answersNode}
+            </div>
+        );
+
+        var max = _.max(cars);
+
+        console.log('max', max );
+
+        var maxObject = '';
+        for (var key in cars) {
+            if (cars[key] == max) maxObject = key;
+        }
+
+        var resultsNode = carName[maxObject];
+
         return (
             <div className="UI">
-                <div className="text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                </div>
+                {isQuizFinished ? resultsNode : questionNode}
             </div>
         );
     }
--- src/proof_of_concept.js
+++ src/proof_of_concept.js
@@ -1,91 +1,19 @@
 "use strict";
 
 var cars = {
-    sitroen:  0,
-    hundai:   0,
-    mersedes: 0,
+    citroen:  0,
+    hyundai:   0,
+    mercedes: 0,
 };
 
-var questions = [{
-    text: "Are you like boolshit?",
-    answers: [{
-        text:   "Yeah! I like it!",
-        points: {
-            sitroen:  20,
-            hundai:   10,
-            mersedes: 0,
-        }
-    }, {
-        text: "Sometimes I like shit, sometimes I like bulls",
-        points: {
-            sitroen:  10,
-            hundai:   0,
-            mersedes: 10,
-        }
-    }, {
-        text: "No!",
-        points: {
-            sitroen:  -5,
-            hundai:   0,
-            mersedes: 10,
-        }
-    }]
-}, {
-    text: "How about putin?",
-    answers: [{
-        text: "Lalalalalalalala!",
-        points: {
-            sitroen:  0,
-            hundai:   10,
-            mersedes: 10,
-        }
-    }, {
-        text: "Huilo!",
-        points: {
-            sitroen:  0,
-            hundai:   10,
-            mersedes: 15,
-        }
-    }, {
-        text: "President of the world!",
-        points: {
-            sitroen:  40,
-            hundai:   0,
-            mersedes: 0,
-        }
-    }, {
-        text: "WTF?",
-        points: {
-            sitroen:  0,
-            hundai:   0,
-            mersedes: 0,
-        }
-    }]
-}, {
-    text: "How mach 9000+1?",
-    answers: [{
-        text: "9001",
-        points: {
-            sitroen:  0,
-            hundai:   0,
-            mersedes: 0,
-        }
-    }, {
-        text: "Over 9000!",
-        points: {
-            sitroen:  0,
-            hundai:   10,
-            mersedes: 0,
-        }
-    }]
-}];
+var questions = require('./questions.json');
 
 for (var i = 0; i < questions.length; i++) {
     var question = questions[i];
 
     var answerIndex = parseInt( prompt([
         question.text,
-        "Ansvers:",
+        "Answers:",
         question.answers.map(function(answer) { return answer.text }).join('\n')
     ].join('\n')) ) -1;
 
